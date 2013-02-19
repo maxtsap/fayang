09096a37aba43a734140888485492e1e35b89386
Maxim Tsaplin <maxim.tsaplin@anahoret.com>
2013-02-19 23:10:43 +0200

    Rails init.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fayang::Application.config.secret_token = 'f13c028c55381275129327a5968415b2bf41438123c48663b8d4d0d67046eb4cf551a850672bd523dc4ebd226da346910f94ef4ca72db4988333ffe404bd9d5a'
